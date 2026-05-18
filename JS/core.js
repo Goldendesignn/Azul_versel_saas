43204877e86846be0f86ccf45412b2159b616af7
Goldendesignn <72046771+Goldendesignn@users.noreply.github.com>
2026-05-19 00:16:40 +0100

    Update core.js

--- JS/core.js
+++ JS/core.js
@@ -2731,7 +2731,9 @@ async function paySelectedConsignmentsInSupabase(ids, paymentLines, actionDate) 
   return true;
 }
 function generateLocalUuid() {
-  if (crypto && crypto.randomUUID) return crypto.randomUUID();
+  if (typeof crypto !== "undefined" && crypto.randomUUID) {
+    return crypto.randomUUID();
+  }
 
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
     var r = Math.random() * 16 | 0;
