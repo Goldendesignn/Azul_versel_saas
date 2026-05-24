bff87f0232de8673a6fd6e9cc892fd9ae5024bd7
Goldendesignn <72046771+Goldendesignn@users.noreply.github.com>
2026-05-24 22:26:24 +0100

    Update push-config.js

--- JS/push-config.js
+++ JS/push-config.js
@@ -1,3 +1,3 @@
 // Cle publique VAPID pour les notifications Web Push.
 // Ne jamais mettre la cle privee ici. La cle privee reste dans Supabase Edge Function Secrets.
-window.AZUL_VAPID_PUBLIC_KEY = "";
+window.AZUL_VAPID_PUBLIC_KEY = "BPb9fyqhLKrz3KNcwoeorz6aoPsmblvzciwl-f2kWBCRJXIUK2RuvMS8UiKaBhOU6xxiaK9M7wKr4dgPMhE-jA0";
