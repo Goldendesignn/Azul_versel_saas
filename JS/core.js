c1538887f751ea1c834aa8ef0ec6ec38cab0d411
Goldendesignn <72046771+Goldendesignn@users.noreply.github.com>
2026-09-25 16:32:26 +0100

    Update core.js

--- JS/core.js
+++ JS/core.js
@@ -3895,7 +3895,7 @@ async function getDashboardDataFromSupabase(filters) {
   var totalLucro = items.reduce(function(sum, item) {
     return sum + (Number(item.profit) || 0);
   }, 0);
-console.log(item);
+console.log(items);
   var topMap = {};
   items.forEach(function(item) {
     var name = item.product_name || "Produto";
